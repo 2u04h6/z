0b0777c187b151ccdfa1f5103c07cc6d45b4b4d4
z <z851017@gmail.com>
2018-01-31 21:22:48 +0800

    1

---
+++ app/Http/Controllers/ZZcontorller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ZZcontorller extends Controller
+{
+    public function index()
+    {
+        return 'pupu';
+    }
+}
---
+++ public/test.php
@@ -0,0 +1,5 @@
+111
+<?php
+	echo $_SERVER ['REQUEST_TIME'] .'<p>';
+	print_r($_SERVER);
+?>
